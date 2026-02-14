b6e1aabf6c5d58b3146330d9fa0bf6bd1c2e59f7
rafe3007 <ramon.fernandezg7@gmail.com>
2026-02-13 23:17:58 -0600

    Add project files.

---
+++ BudgetUtils/Config/AppSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/* 
+ * Making a class is necessary to support strongly typed configuration. 
+ * This class will be used to bind the configuration values from appsettings.json or other configuration sources.
+ */ 
+
+namespace BudgetUtils.Config
+{
+    class AppSettings
+    {
+        public required DateRange DateRange { get; set; }
+        public required string OutputPath { get; set; }
+        public required string InputPath { get; set; }
+    }
+
+    class DateRange
+    {
+        public required int Interval { get; set; }
+        public required string Unit { get; set; }
+        public string TimeString() {
+            int interval = Interval;
+            string u = Unit.ToLower();
+            return u switch
+            {
+                "seconds" => $"{interval} Seconds(s)",
+                "minutes" => $"{interval} Minutes(s)",
+                "hours" => $"{interval} Hours(s)",
+                _ => $"BAD UNIT: {u}"
+            };
+        }
+    }
+}
---
+++ BudgetUtils/Program.cs
@@ -0,0 +1,24 @@
+﻿using BudgetUtils.Config;
+using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var appSettings = configuration.GetSection("BudgetUtils").Get<AppSettings>();
+
+if (appSettings == null)
+{
+    Markup.FromInterpolated($"[red]appSettings is null, please ensure it is properly configured");
+    return;
+}
+
+// TODO: make table display of steps and progress bar
+AnsiConsole.MarkupLine($"[DarkSlateGray2]Fetching all transaction .csv files from[/]" +
+    $"[yellow]{appSettings.InputPath}[/] " +
+    $"[DarkSlateGray2]for the last[/] " +
+    $"[yellow]{appSettings.DateRange.TimeString()}" +
+    $"[/][DarkSlateGray2]...[/]");
+
